6729f49cd31199539c2dfbab2c9346853a98c04a
Zenith <zenitharryan@gmail.com>
2023-03-08 12:21:35 +0700

    initial commit

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -15,21 +15,21 @@ describe('HTTP server', () => {
     await AuthenticationsTableTestHelper.cleanTable();
   });
 
-  describe('when GET /', () => {
-    it('should return 200 and hello world', async () => {
-      // Arrange
-      const server = await createServer({});
-      // Action
-      const response = await server.inject({
-        method: 'GET',
-        url: '/',
-      });
-      // Assert
-      const responseJson = JSON.parse(response.payload);
-      expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello world!');
-    });
-  });
+  // describe('when GET /', () => {
+  //   it('should return 200 and hello world', async () => {
+  //     // Arrange
+  //     const server = await createServer({});
+  //     // Action
+  //     const response = await server.inject({
+  //       method: 'GET',
+  //       url: '/',
+  //     });
+  //     // Assert
+  //     const responseJson = JSON.parse(response.payload);
+  //     expect(response.statusCode).toEqual(200);
+  //     expect(responseJson.value).toEqual('Hello world!');
+  //   });
+  // });
 
   it('should response 404 when request unregistered route', async () => {
     // Arrange
